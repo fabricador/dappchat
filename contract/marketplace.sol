9923c35ac97712672d5fa77bac324518144a92a2
fabricador <fabricador@protonmail.com>
2021-12-28 19:37:17 -0600

    first commit

---
+++ contract/marketplace.sol
@@ -0,0 +1,136 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 <0.9.0;
+
+interface IERC20Token {
+    function transfer(address, uint256) external returns (bool);
+
+    function approve(address, uint256) external returns (bool);
+
+    function transferFrom(
+        address,
+        address,
+        uint256
+    ) external returns (bool);
+
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address) external view returns (uint256);
+
+    function allowance(address, address) external view returns (uint256);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+}
+
+contract ChatGroups {
+    uint256 internal chatsLength = 0;
+    uint256 internal messagesLength = 0;
+    address internal cUsdTokenAddress =
+        0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
+
+    struct Message {
+        address creator;
+        string content;
+    }
+
+    struct Chat {
+        address payable owner;
+        string title;
+        string description;
+        uint256 price;
+        address[] participants;
+        uint256[] messages;
+    }
+
+    mapping(uint256 => Chat) internal chats;
+
+    mapping(uint256 => Message) internal messages;
+
+    function createChat(
+        string memory _title,
+        string memory _description,
+        uint256 _price
+    ) public {
+        address[] memory _participants;
+        uint256[] memory _messages;
+        chats[chatsLength] = Chat(
+            payable(msg.sender),
+            _title,
+            _description,
+            _price,
+            _participants,
+            _messages
+        );
+        chats[chatsLength].participants.push(msg.sender);
+        chatsLength++;
+    }
+
+    function sendMessage(uint256 _index, string memory _content) public {
+        chats[_index].messages.push(messagesLength);
+        messages[messagesLength] = Message(msg.sender, _content);
+        messagesLength++;
+    }
+
+    function getChat(uint256 _index)
+        public
+        view
+        returns (
+            address payable,
+            string memory,
+            string memory,
+            uint256,
+            address[] memory,
+            uint256[] memory
+        )
+    {
+        return (
+            chats[_index].owner,
+            chats[_index].title,
+            chats[_index].description,
+            chats[_index].price,
+            chats[_index].participants,
+            chats[_index].messages
+        );
+    }
+
+    function getMessages(uint256 _index)
+        public
+        view
+        returns (uint256[] memory)
+    {
+        return (chats[_index].messages);
+    }
+
+    function getMessage(uint256 _index)
+        public
+        view
+        returns (address, string memory)
+    {
+        return (messages[_index].creator, messages[_index].content);
+    }
+
+    function joinChat(uint256 _index) public payable {
+        require(
+            IERC20Token(cUsdTokenAddress).transferFrom(
+                msg.sender,
+                chats[_index].owner,
+                chats[_index].price
+            ),
+            "Transfer failed."
+        );
+        chats[_index].participants.push(msg.sender);
+    }
+
+    function getChatsLength() public view returns (uint256) {
+        return (chatsLength);
+    }
+
+    function getMessagesLength() public view returns (uint256) {
+        return (messagesLength);
+    }
+}
---
+++ src/main.js
@@ -0,0 +1,268 @@
+import Web3 from "web3"
+import { newKitFromWeb3 } from "@celo/contractkit"
+import BigNumber from "bignumber.js"
+import marketplaceAbi from "../contract/marketplace.abi.json"
+import erc20Abi from "../contract/erc20.abi.json"
+
+const ERC20_DECIMALS = 18
+const MPContractAddress = "0xc6Ad961570151f262029997d53E70c6B2B6599d7"
+const cUSDContractAddress = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1"
+
+let kit
+let contract
+let chats = []
+let messages = []
+let chatId
+
+const connectCeloWallet = async function () {
+  if (window.celo) {
+    notification("⚠️ Please approve this DApp to use it.")
+    try {
+      await window.celo.enable()
+      notificationOff()
+
+      const web3 = new Web3(window.celo)
+      kit = newKitFromWeb3(web3)
+
+      const accounts = await kit.web3.eth.getAccounts()
+      kit.defaultAccount = accounts[0]
+
+      contract = new kit.web3.eth.Contract(marketplaceAbi, MPContractAddress)
+    } catch (error) {
+      notification(`⚠️ ${error}.`)
+    }
+  } else {
+    notification("⚠️ Please install the CeloExtensionWallet.")
+  }
+}
+
+async function approve(_price) {
+  const cUSDContract = new kit.web3.eth.Contract(erc20Abi, cUSDContractAddress)
+
+  const result = await cUSDContract.methods
+    .approve(MPContractAddress, _price)
+    .send({ from: kit.defaultAccount })
+  return result
+}
+
+const getBalance = async function () {
+  const totalBalance = await kit.getTotalBalance(kit.defaultAccount)
+  const cUSDBalance = totalBalance.cUSD.shiftedBy(-ERC20_DECIMALS).toFixed(2)
+  document.querySelector("#balance").textContent = cUSDBalance
+}
+
+const getChats = async function() {
+  const _chatsLength = await contract.methods.getChatsLength().call()
+  const _chats = []
+  for (let i = 0; i < _chatsLength; i++) {
+    let _chat = new Promise(async (resolve, reject) => {
+      let c = await contract.methods.getChat(i).call()
+      resolve({
+        index: i,
+        owner: c[0],
+        title: c[1],
+        description: c[2],
+        price: new BigNumber(c[3]),
+        participans: c[4],
+        messages: c[5],
+      })
+    })
+    _chats.push(_chat)
+  }
+  chats = await Promise.all(_chats)
+  renderChats()
+}
+
+function renderChats() {
+  document.getElementById("chatsList").innerHTML = ""
+  chats.forEach((_chat) => {
+    const chatElement = _chat.participans.includes(kit.defaultAccount) ? viewChatTemplate(_chat) : joinChatTemplate(_chat)
+    document.getElementById("chatsList").innerHTML += chatElement
+  })
+}
+
+function joinChatTemplate(_chat) {
+  return `
+  <a class="list-group-item list-group-item-action rounded-0 joinChat" style="cursor: pointer;" id="${_chat.index}">
+      <div class="media-body ml-4 joinChat" id="${_chat.index}">
+        <div class="d-flex align-items-center justify-content-between mb-1 joinChat" id="${_chat.index}">
+          <h6 class="mb-0 joinChat" id="${_chat.index}">${_chat.title} - ${_chat.price.shiftedBy(-ERC20_DECIMALS).toFixed(2)} cUSD</h6>
+        </div>
+        <p class="font-italic mb-0 text-small joinChat" id="${_chat.index}">${_chat.description}</p>
+      </div>
+  </a>
+  `
+}
+
+function viewChatTemplate(_chat) {
+  return `
+  <a class="list-group-item list-group-item-action rounded-0 viewChat" style="cursor: pointer;" id="${_chat.index}">
+      <div class="media-body ml-4 viewChat" id="${_chat.index}">
+        <div class="d-flex align-items-center justify-content-between mb-1 viewChat" id="${_chat.index}">
+          <h6 class="mb-0 viewChat" id="${_chat.index}">${_chat.title} - Joined</h6>
+        </div>
+        <p class="font-italic mb-0 text-small viewChat" id="${_chat.index}">${_chat.description}</p>
+      </div>
+  </a>
+  `
+}
+
+async function getMessages() {
+  const messagesIndexes = await contract.methods.getMessages(chatId).call()
+  let _messages = []
+
+  for (let i of messagesIndexes) {
+    let _message = new Promise(async (resolve, reject) => {
+      let m = await contract.methods.getMessage(i).call()
+      resolve({
+        index: i,
+        creator: m[0],
+        content: m[1],
+      })
+    })
+    _messages.push(_message)
+  }
+
+  messages = await Promise.all(_messages)
+  
+  renderMessages()
+}
+
+function renderMessages(){
+  document.getElementById("chatBox").innerHTML = ""
+  messages.forEach((_message) => {
+    const messageElement = _message.creator == kit.defaultAccount ? seMessageTemplate(_message) : reMessageTemplate(_message)
+    document.getElementById("chatBox").innerHTML += messageElement
+  })
+}
+
+function reMessageTemplate(_message){
+  return `
+  <div class="media w-50 mb-3">${identiconTemplate(_message.creator)}
+    <div class="media-body ml-3">
+      <div class="bg-light rounded py-2 px-3 mb-2">
+        <p class="text-small mb-0 text-muted">${_message.content}</p>
+      </div>
+      <p class="small text-muted">${_message.creator}</p>
+    </div>
+  </div>
+  `
+}
+
+function seMessageTemplate(_message){
+  return `
+  <div class="media w-50 ml-auto mb-3">
+    <div class="media-body">
+      <div class="bg-primary rounded py-2 px-3 mb-2">
+        <p class="text-small mb-0 text-white">${_message.content}</p>
+      </div>
+      <p style="font-size: 10px" class="small text-muted">${_message.creator}</p>
+    </div>
+  </div>
+  `
+}
+
+function identiconTemplate(_address) {
+  const icon = blockies
+    .create({
+      seed: _address,
+      size: 8,
+      scale: 16,
+    })
+    .toDataURL()
+
+  return `
+  <div>
+    <a href="https://alfajores-blockscout.celo-testnet.org/address/${_address}/transactions"
+        target="_blank">
+        <img src="${icon}" width="48" alt="${_address}" style="border-radius:24px;">
+    </a>
+  </div>
+  `
+}
+
+function notification(_text) {
+  document.querySelector(".alert").style.display = "block"
+  document.querySelector("#notification").textContent = _text
+}
+
+function notificationOff() {
+  document.querySelector(".alert").style.display = "none"
+}
+
+window.addEventListener("load", async () => {
+  notification("⌛ Loading...")
+  await connectCeloWallet()
+  await getBalance()
+  await getChats()
+  notificationOff()
+});
+
+document
+  .querySelector("#newChatBtn")
+  .addEventListener("click", async (e) => {
+    const params = [
+      document.getElementById("nameInput").value,
+      document.getElementById("descriptionInput").value,
+      new BigNumber(document.getElementById("priceInput").value)
+      .shiftedBy(ERC20_DECIMALS)
+      .toString()
+    ]
+    notification(`⌛ Adding "${params[0]}"...`)
+    try {
+      const result = await contract.methods
+        .createChat(...params)
+        .send({ from: kit.defaultAccount })
+    } catch (error) {
+      notification(`⚠️ ${error}.`)
+    }
+    notification(`🎉 You successfully added "${params[0]}".`)
+    getChats()
+  })
+
+document.querySelector("#chatsList").addEventListener("click", async (e) => {
+  if (e.target.className.includes("joinChat")) {
+    const index = e.target.id
+    chatId = index
+    notification("⌛ Waiting for payment approval...")
+    try {
+      await approve(chats[index].price)
+    } catch (error) {
+      notification(`⚠️ ${error}.`)
+    }
+    notification(`⌛ Awaiting payment for "${chats[index].title}"...`)
+    try {
+      const result = await contract.methods
+        .joinChat(index)
+        .send({ from: kit.defaultAccount })
+      notification(`🎉 You successfully joined "${chats[index].title}".`)
+      getChats()
+      getBalance()
+    } catch (error) {
+      notification(`⚠️ ${error}.`)
+    }
+    getMessages()
+  }
+  if (e.target.className.includes("viewChat")){
+    chatId = e.target.id
+    getMessages()
+  }
+})  
+
+document.getElementById("sendMessage").addEventListener("click", async (e) => {
+  const params = [
+    chatId,
+    document.getElementById("messageContent").value
+  ]
+  notification(`⌛ Sending message...`)
+  try {
+    const result = await contract.methods
+      .sendMessage(...params)
+      .send({ from: kit.defaultAccount })
+  } catch (error) {
+    notification(`⚠️ ${error}.`)
+  }
+  document.getElementById("messageContent").value = ""
+  getMessages()
+  notification(`🎉 Message sent successfully.`)
+})
