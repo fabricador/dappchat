0fe6dc7ec4b31de3d7fb4af04db6e192ede36c81
= <Emmanuelsumeh@gmail.com>
2022-01-04 23:07:50 +0100

    feat: fixed vulnerability and optimized smart contract

---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+const ERC20_DECIMALS = 18
+const MPContractAddress = "0xc6Ad961570151f262029997d53E70c6B2B6599d7"
+const cUSDContractAddress = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1"
+
+
+export {ERC20_DECIMALS, MPContractAddress, cUSDContractAddress}
